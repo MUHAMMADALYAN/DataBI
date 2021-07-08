e324be66f275c3ba8af0c79474410d10817a1288
MUHAMMAD ALYAN <malyan.bese18seecs@seecs.du.pk>
2021-07-09 00:43:07 +0500

    requirements file

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xifhf+w)s63s8fowg==tpkecr_8q92*6f4%veyns17e^_@ro8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
